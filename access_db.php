8de2ee10a6ec311b503a074d5f28640d50b98762
Turnemo <136558926+Turnemo@users.noreply.github.com>
2023-06-23 00:49:57 +0700

    Update access_db.php

--- access_db.php
+++ access_db.php
@@ -1,16 +1,26 @@
+
 <?php
+// подключение к базе данных
 $db_path = 'Turnemo/turnemo.github.io/database.accdb';
 $conn = new COM('ADODB.Connection');
 $conn->Open("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=$db_path");
 
-
+// выполнение запроса
 $rs = $conn->Execute("SELECT * FROM table_name");
+
+// вывод результатов
+echo "<table>";
 while (!$rs->EOF) {
-    echo $rs->Fields['column_name']->Value;
+    echo "<tr>";
+    echo "<td>" . $rs->Fields['column_1']->Value . "</td>";
+    echo "<td>" . $rs->Fields['column_2']->Value . "</td>";
+    // добавьте столбцы, которые нужно отобразить
+    echo "</tr>";
     $rs->MoveNext();
 }
+echo "</table>";
 
-
+// закрытие соединения
 $rs->Close();
 $conn->Close();
 ?>
